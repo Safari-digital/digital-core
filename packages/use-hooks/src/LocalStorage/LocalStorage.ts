5793ea32e549ce20d7a421a7af70ac5c542d2696
BenoitSafari <gregory.michalak.192@gmail.com>
2024-09-11 00:08:27 +0100

    release/0.1.1@use-hooks: type fixes

--- packages/use-hooks/src/LocalStorage/LocalStorage.ts
+++ packages/use-hooks/src/LocalStorage/LocalStorage.ts
@@ -1,13 +1,13 @@
 import '@safari-node/core';
 
 export default class LocalStorage {
-    public static get<T>(key: string | null | undefined): T | null | undefined {
+    public static get<T>(key: string | null | undefined): T | null {
         const value = localStorage.getItem(key ?? '');
         return JSON.safeParse(value ?? '');
     }
 
     public static set(key: string | null | undefined, value: any): void {
-        key && localStorage.setItem(key, JSON.stringify(value));
+        if (key) localStorage.setItem(key, JSON.stringify(value));
     }
 
     public static remove(...keys: Array<string | null | undefined>): void {
